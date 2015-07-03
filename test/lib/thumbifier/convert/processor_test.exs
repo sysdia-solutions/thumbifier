790769240b0486f3a89018dfb07b0bbd3a7fda27
Harvey Ball <me@harveyball.com>
2015-07-03 07:41:40 +0100

    refactor(Processor): generate expected image output `on-the-fly`
    Previously the expected image output was asserted against a fixture file
    by comparing the base64 encoded string of the fixture file and the image
    file generated by the Processor.
    
    This is too inconsistent as the version of ImageMagick will generate a
    different file structure to the expected image output and therefore
    there is a chance the base64 encoded version of the files may not match
    even though the images look the same.
    
    This solution uses the local fixture file to generate the expected
    output to ensure the assertion is always identical.
    
    Whilst, it doesn't prove the image is generated correctly as it will
    generate the same base64 signature even on error, it at least determines
    if the output sent to the callback url is well formed.
    
    Testing the actual image output should be done via an end-to-end test
    and using image comparison (such as ImageMagick compare function) to
    ensure visual accuracy.

--- test/lib/thumbifier/convert/processor_test.exs
+++ test/lib/thumbifier/convert/processor_test.exs
@@ -20,6 +20,13 @@ defmodule ProcessorTest do
     {:ok, data: data, remote_fixtures_path: remote_fixtures_path}
   end
 
+  defp process_expected(data) do
+    path = elem(data, 1)
+    expected_image = File.read!(path) |> Base.encode64
+    File.rm!(path)
+    expected_image
+  end
+
   test "process - posts error when provided media_url is too large", %{data: data} do
     with_mock HTTPoison, [post: fn(_data, _callback_url) -> :ok end] do
       Thumbifier.Convert.Processor.process(1, data)
@@ -60,6 +67,10 @@ defmodule ProcessorTest do
       "dimensions" => "250x250"
       })
 
+    expected_image = System.cwd <> "/test/fixtures/files/tiff.tiff"
+    |> Thumbifier.Convert.Converter.resize(data["quality"], data["dimensions"])
+    |> process_expected
+
     with_mock HTTPoison, [post: fn(_data, _callback_url) -> :ok end] do
       Application.get_env(:thumbifier, :max_file_size)
       |> Thumbifier.Convert.Processor.process(data)
@@ -69,7 +80,7 @@ defmodule ProcessorTest do
           status: "success",
           response_id: data["response_id"],
           personal_reference: data["personal_reference"],
-          payload: System.cwd <> "/test/fixtures/files/tiff_thumb.jpg" |> File.read! |> Base.encode64
+          payload: expected_image
         ]
       }
       assert called HTTPoison.post(data["callback_url"], form)
@@ -79,6 +90,10 @@ defmodule ProcessorTest do
   test "process - success for png file format", %{data: data, remote_fixtures_path: remote_fixtures_path} do
     data = Map.merge(data, %{"media_url" => remote_fixtures_path <> "png.png"})
 
+    expected_image = System.cwd <> "/test/fixtures/files/png.png"
+    |> Thumbifier.Convert.Converter.resize(data["quality"], data["dimensions"])
+    |> process_expected
+
     with_mock HTTPoison, [post: fn(_data, _callback_url) -> :ok end] do
       Application.get_env(:thumbifier, :max_file_size)
       |> Thumbifier.Convert.Processor.process(data)
@@ -88,7 +103,7 @@ defmodule ProcessorTest do
           status: "success",
           response_id: data["response_id"],
           personal_reference: data["personal_reference"],
-          payload: System.cwd <> "/test/fixtures/files/png_thumb.jpg" |> File.read! |> Base.encode64
+          payload: expected_image
         ]
       }
       assert called HTTPoison.post(data["callback_url"], form)
@@ -98,6 +113,10 @@ defmodule ProcessorTest do
   test "process - success for gif file format", %{data: data, remote_fixtures_path: remote_fixtures_path} do
     data = Map.merge(data, %{"media_url" => remote_fixtures_path <> "gif.gif"})
 
+    expected_image = System.cwd <> "/test/fixtures/files/gif.gif"
+    |> Thumbifier.Convert.Converter.resize(data["quality"], data["dimensions"])
+    |> process_expected
+
     with_mock HTTPoison, [post: fn(_data, _callback_url) -> :ok end] do
       Application.get_env(:thumbifier, :max_file_size)
       |> Thumbifier.Convert.Processor.process(data)
@@ -107,7 +126,7 @@ defmodule ProcessorTest do
           status: "success",
           response_id: data["response_id"],
           personal_reference: data["personal_reference"],
-          payload: System.cwd <> "/test/fixtures/files/gif_thumb.jpg" |> File.read! |> Base.encode64
+          payload: expected_image
         ]
       }
       assert called HTTPoison.post(data["callback_url"], form)
@@ -117,6 +136,10 @@ defmodule ProcessorTest do
   test "process - success for bmp file format", %{data: data, remote_fixtures_path: remote_fixtures_path} do
     data = Map.merge(data, %{"media_url" => remote_fixtures_path <> "bmp.bmp"})
 
+    expected_image = System.cwd <> "/test/fixtures/files/bmp.bmp"
+    |> Thumbifier.Convert.Converter.resize(data["quality"], data["dimensions"])
+    |> process_expected
+
     with_mock HTTPoison, [post: fn(_data, _callback_url) -> :ok end] do
       Application.get_env(:thumbifier, :max_file_size)
       |> Thumbifier.Convert.Processor.process(data)
@@ -126,7 +149,30 @@ defmodule ProcessorTest do
           status: "success",
           response_id: data["response_id"],
           personal_reference: data["personal_reference"],
-          payload: System.cwd <> "/test/fixtures/files/bmp_thumb.jpg" |> File.read! |> Base.encode64
+          payload: expected_image
+        ]
+      }
+      assert called HTTPoison.post(data["callback_url"], form)
+    end
+  end
+
+  test "process - success for pdf file format", %{data: data, remote_fixtures_path: remote_fixtures_path} do
+    data = Map.merge(data, %{"media_url" => remote_fixtures_path <> "pdf.pdf"})
+
+    expected_image = System.cwd <> "/test/fixtures/files/pdf.pdf"
+    |> Thumbifier.Convert.Converter.from_pdf(data["quality"], data["dimensions"], data["page"])
+    |> process_expected
+
+    with_mock HTTPoison, [post: fn(_data, _callback_url) -> :ok end] do
+      Application.get_env(:thumbifier, :max_file_size)
+      |> Thumbifier.Convert.Processor.process(data)
+
+      form = { :form,
+        [
+          status: "success",
+          response_id: data["response_id"],
+          personal_reference: data["personal_reference"],
+          payload: expected_image
         ]
       }
       assert called HTTPoison.post(data["callback_url"], form)
